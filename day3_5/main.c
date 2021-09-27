3c4fe03c500dc651f8c111ee50d6d6fd78888d16
Adil Rasheed <ibnerasheed@gmail.com>
2021-09-27 20:05:27 +0530

    power function and factorial

--- day3_5/main.c
+++ day3_5/main.c
@@ -1,8 +1,24 @@
+/*Write a recursive function for calculating power of a number. Take base number and exponent from user.*/
+
 #include <stdio.h>
-#include <stdlib.h>
+
+int power(int base, int exp)
+{
+    if (exp != 0)
+        return (base * power(base, exp - 1));
+    else
+        return 1;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    int base, exponent, result;
+    printf("Enter base number: ");
+    scanf("%d", &base);
+    printf("Enter power number(positive integer): ");
+    scanf("%d", &exponent);
+    result = power(base, exponent);
+    printf("%d^%d = %d", base, exponent, result);
+
     return 0;
 }
